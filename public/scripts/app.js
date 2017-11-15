19ed13f46d201c87fa1f3064ae1b6ac35c85eead
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-15 04:01:54 +0000

    runs through the tweet array and appends them into their own article

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,79 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+function createTweetElement(tweetData) {
+    var newTweet = $('<article>').append($('<header>'));
+
+    var header = newTweet.children('header');
+    header.append($('<img />', { src: tweetData.user.avatars.small, class: 'tweet-image', alt: 'MyAlt' }));
+    header.append($('<div class="tweet-name">').text(tweetData.user.name));
+    header.append($('<div class="tweet-id">').text(tweetData.user.handle));
+
+    newTweet.append($('<div class="written-tweet">').text(tweetData.content.text));
+
+    newTweet.append($('<footer class="clearfix">'));
+    var footer = newTweet.children('footer');
+    footer.append($('<div class="time">').text(tweetData.created_at));
+    footer.append($('<div class="icons">'));
+
+   return newTweet;
+}
+
+function renderTweets(tweets) {
+  for(var tweet in tweets) {
+    var createdTweet = createTweetElement(tweets[tweet]);
+    $('#created-tweet').append(createdTweet);
+  }
+  return createdTweet;
+}
+
+$(document).ready(function() {
+  var tweet = renderTweets(data);
+});
